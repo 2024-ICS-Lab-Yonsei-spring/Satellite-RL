bc9484c33ed6de7f9ddd99c3cccae46c18a5c3da
qotmdgh <qotmdgh0903@yonsei.ac.kr>
2024-06-09 20:29:06 +0900

    Delete Beamhopping/swResetFunction.m

--- Beamhopping/swResetFunction.m
+++
@@ -1,8 +0,0 @@
-function [InitialObservation, InitialState] = swResetFunction()
-    % Initialize data traffic demand and received data traffic
-    numCells = 57;
-    dataTrafficDemand = randi([1, 10], numCells, 1); % Initial demand for each cell
-    receivedDataTraffic = zeros(numCells, 1); % Initial received traffic for each cell
-    InitialState = [dataTrafficDemand; receivedDataTraffic];
-    InitialObservation = dataTrafficDemand ./ (receivedDataTraffic + 1); % Initial state
-end
